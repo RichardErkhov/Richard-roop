47cb266eb6074824e1c7466b81f1c8103953af88
Janvarev Vladislav <janvarev@gmail.com>
2023-06-15 18:42:40 +0300

    cleaner plugin_gfpgan.py, added adequate multithread

--- plugins/plugin_gfpgan.py
+++ plugins/plugin_gfpgan.py
@@ -12,10 +12,6 @@ import threading
 
 modname = os.path.basename(__file__)[:-3] # calculating modname
 
-FACE_ENHANCER = None
-THREAD_SEMAPHORE = threading.Semaphore()
-THREAD_LOCK = threading.Lock()
-
 # start function
 def start(core:ChainImgProcessor):
     manifest = { # plugin settings
@@ -42,35 +38,27 @@ def conditional_download(download_directory_path: str, urls: List[str]) -> None:
             with tqdm(total=total, desc='Downloading', unit='B', unit_scale=True, unit_divisor=1024) as progress:
                 urllib.request.urlretrieve(url, download_file_path, reporthook=lambda count, block_size, total_size: progress.update(block_size))
 
-def get_face_enhancer() -> None:
-    import gfpgan
-    global FACE_ENHANCER
-
-    with THREAD_LOCK:
-        if FACE_ENHANCER is None:
-            model_path = './models/GFPGANv1.3.pth'
-            FACE_ENHANCER = gfpgan.GFPGANer(
-                model_path=model_path,
-                channel_multiplier=2,
-                upscale=2
-            )
-    return FACE_ENHANCER
-
-def enhance_face(temp_frame: Any) -> Any:
-    THREAD_SEMAPHORE.acquire()
-    _, _, temp_frame = get_face_enhancer().enhance(
-        temp_frame,
-        paste_back=True
-    )
-    THREAD_SEMAPHORE.release()
-    return temp_frame
-
 class PluginGfpgan(ChainImgPlugin):
     def init_plugin(self):
         import gfpgan
         conditional_download("./models",
                              ['https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth'])
         #pass
+        model_path = './models/GFPGANv1.3.pth'
+        self.face_enchancer = gfpgan.GFPGANer(
+            model_path=model_path,
+            channel_multiplier=2,
+            upscale=2
+        )
+
+    def enhance_face(self, temp_frame: Any) -> Any:
+        #THREAD_SEMAPHORE.acquire()
+        _, _, temp_frame = self.face_enchancer.enhance(
+            temp_frame,
+            paste_back=True
+        )
+        #THREAD_SEMAPHORE.release()
+        return temp_frame
 
     def process(self, frame, params:dict):
         # params can be used to transfer some img info to next processors
@@ -84,7 +72,7 @@ class PluginGfpgan(ChainImgPlugin):
 
         face = get_face_single(frame)
         if face:
-            frame = enhance_face(frame)
+            frame = self.enhance_face(frame) # this double image size
             frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
             yes_face = True
 
